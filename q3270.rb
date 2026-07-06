871c466e0e46258c78171fd4dd2e17e6ae6cd3ed
HobbitHacker <81963937+HobbitHacker@users.noreply.github.com>
2026-07-06 18:55:51 +0100

    Update Q3270 to use Qt 6 instead of Qt 5

--- q3270.rb
+++ q3270.rb
@@ -1,27 +1,22 @@
 class Q3270 < Formula
   desc "Qt-based 3270 Terminal Emulator"
   homepage "https://github.com/hobbithacker/Q3270"
-  # You will update this URL and SHA whenever you release a new version
   url "https://github.com/HobbitHacker/Q3270/archive/refs/tags/v0.3.0.tar.gz"
   sha256 "1d2058af95e62bc494b444b9ec71abac731fd6a7647a704213527dffeadd3b1a"
-
   depends_on "cmake" => :build
-  depends_on "qt@5"
+  depends_on "qt@6"
 
   def install
-    # Homebrew versions of Qt5 are 'keg-only', so we must point CMake to them
-    qt5 = Formula["qt@5"].opt_prefix
-    
-    # Detect if we are on Intel or ARM
+    qt6 = Formula["qt@6"].opt_prefix
+
     arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
-    
+
     mkdir "build" do
       system "cmake", "..", *std_cmake_args,
-             "-DCMAKE_PREFIX_PATH=#{qt5}",
-             "-DCMAKE_OSX_ARCHITECTURES=#{arch}" # <--- Use the detected arch
+             "-DCMAKE_PREFIX_PATH=#{qt6}",
+             "-DCMAKE_OSX_ARCHITECTURES=#{arch}"
       system "make", "-j#{ENV.make_jobs}"
-      
-      # Install the bundle into the Homebrew prefix
+
       prefix.install "src/Q3270.app"
     end
   end
@@ -30,7 +25,6 @@ class Q3270 < Formula
     <<~EOS
       Q3270.app has been installed to:
         #{opt_prefix}/Q3270.app
-
       To link it to your Applications folder, run:
         ln -s #{opt_prefix}/Q3270.app /Applications/
     EOS
